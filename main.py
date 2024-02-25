8f2d3aa360f81c7765bbd5ffedb91142626bf43e
NBWolfer <32mahmutenes32@gmail.com>
2024-02-25 15:23:24 +0300

    ...

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from os import getenv
 if __name__ == "__main__":
     port = int(getenv("PORT", 8000))
     uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
+
+# GitHubı anlama çabaları
